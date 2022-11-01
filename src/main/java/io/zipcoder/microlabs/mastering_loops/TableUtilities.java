79c641e12701436a9c3230beb207fba58bcafa8e
Collin <collin@Zipcoders-MacBook-Pro.local>
2022-11-01 13:23:17 -0400

    all tests passed

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,35 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String str = "";
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++){
+                str +=String.format("%3d |", i * j);
+            }
+            str += "\n";
+        }
+        return str;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String str = "";
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++){
+                str +=String.format("%3d |", i * j);
+            }
+            str += "\n";
+        }
+        return str;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String str = "";
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 1; j <= tableSize; j++){
+                str +=String.format("%3d |", i * j);
+            }
+            str += "\n";
+        }
+        return str;
     }
 }
--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,18 +3,42 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String x = "";
+        for (int i = 1; i <= numberOfRows - 1; i++) {
+            for (int j = 1; j <= i; j++) {
+                x += "*";
+                }
+            x += "\n";
+            }
+        return x;
     }
+        public static String getRow(int width){
+        String x = "";
+        for (int i = 0; i < width; i++) {
+                x += "*";
+            }
+            return x;
+        }
 
-    public static String getRow(int numberOfStars) {
-        return null;
-    }
+        public static String getSmallTriangle () {
+            String x = "";
+            for (int i = 1; i <= 4; i++) {
+                for (int j = 1; j <= i; j++) {
+                    x += "*";
+                }
+                x += "\n";
+            }
+            return x;
+        }
 
-    public static String getSmallTriangle() {
-        return null;
-    }
-
-    public static String getLargeTriangle() {
-        return null;
-    }
+        public static String getLargeTriangle () {
+            String x = "";
+            for (int i = 1; i <= 9; i++) {
+                for (int j = 1; j <= i; j++) {
+                    x += "*";
+                }
+                x += "\n";
+            }
+            return x;
+        }
 }
